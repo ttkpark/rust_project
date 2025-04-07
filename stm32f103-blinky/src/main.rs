4e688d775f79847cc6fc0fbd09c7cf98fe3c39c1
Giho Park <ttkpark0124@gmail.com>
2025-04-07 22:59:43 +0900

    day 6 commit not ended

--- stm32f103-blinky/src/main.rs
+++ stm32f103-blinky/src/main.rs
@@ -5,13 +5,15 @@ use cortex_m_rt::entry;
 use panic_halt as _;
 
 use stm32f1xx_hal::{
-    pac,
-    prelude::*, // GPIO 설정
-    timer::Timer, // delay
-    gpio::gpioc::PC13,
-    gpio::{Output, PushPull},
-    time::U32Ext
+    gpio::{Output, PushPull, *},
+    pac::{self, usart1}, 
+    prelude::*, 
+    serial::{Config, Serial}, 
+    timer::Timer
 };
+
+use core::fmt::Write; //write! 매크로 사용
+use nb::block;
 
 #[entry]
 fn main() -> ! {
@@ -34,6 +36,27 @@ fn main() -> ! {
     let mut gpioc = dp.GPIOC.split();
     let mut led: PC13<Output<PushPull>> = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
+    // GPIO 설정
+    let mut afio = dp.AFIO.constrain();
+    let mut gpioa = dp.GPIOA.split();
+
+    //PA9 = TX, PA10 = RX
+    let pin_tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+    let pin_rx  = gpioa.pa10;
+
+    // UART 설정
+    let serial = Serial::new(
+        dp.USART1,
+        (pin_tx,pin_rx),
+        &mut afio.mapr,
+        Config::default()
+            .baudrate(115_200.bps())
+            .wordlength_8bits()
+            .parity_none(),
+        &clocks
+    );
+    let (mut tx, _rx) = serial.split();
+    
     // SYST 타이머 기반 딜레이 생성
     let mut delay = Timer::syst(cp.SYST, &clocks).delay();
 
@@ -43,5 +66,8 @@ fn main() -> ! {
 
         led.set_high();   // 끄기
         delay.delay_ms(500u16);
+        
+        
+        writeln!(tx, "hello, Rust from STM32!").unwrap()
     }
 }
